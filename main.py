4d2062277220ef7dcd42c9854ea74f0d54fa7961
AM-Turing <shirley780@live.com>
2025-04-13 18:59:46 -0400

    Create groups container in main.py

--- main.py
+++ main.py
@@ -11,15 +11,21 @@ def main():
     dt = 0
     x = SCREEN_WIDTH // 2
     y = SCREEN_HEIGHT // 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        # player.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        # player.draw(screen)
         pygame.display.flip()
 
         # Limit framerate to 60fps
