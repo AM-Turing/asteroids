89da22fb9d66bfd4f2b4fde05b4f90d15c46fd17
AM-Turing <shirley780@live.com>
2025-04-13 18:27:33 -0400

    Create CicleShape and Player modules

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black = (0, 0, 0)
+    dt = 0
+    x = SCREEN_WIDTH // 2
+    y = SCREEN_HEIGHT // 2
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(black, rect=None, special_flags=0)
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # Limit framerate to 60fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
