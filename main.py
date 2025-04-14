5766fce330c8f7740390705d89185ba5f43950a1
AM-Turing <shirley780@live.com>
2025-04-14 13:12:33 -0400

    Add asteroid and collision logic

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    black = (0, 0, 0)
     dt = 0
     x = SCREEN_WIDTH // 2
     y = SCREEN_HEIGHT // 2
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
     player = Player(x, y)
 
     while True:
@@ -26,6 +32,10 @@ def main():
         for sprite in drawable:
             sprite.draw(screen)
         # player.draw(screen)
+        for object in asteroids:
+            if object.collisions(player):
+                print("Game over!")
+                sys.exit()
         pygame.display.flip()
 
         # Limit framerate to 60fps
